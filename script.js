8eeaa716e1a35a7e293975abe30bb004ef264ad9
rayannfoo <you@example.com>
2023-11-30 15:25:21 +0800

    update

--- script.js
+++ script.js
@@ -19,9 +19,33 @@ function viewCart() {
     }
 }
 
+function checkCart() {
+    if (cart.length === 0) {
+        alert('Cart is empty. Please add items before proceeding to checkout.');
+        // Prevent the default behavior of the link (going to checkout.html)
+        event.preventDefault();
+    }
+}
+
 /* dropdown for members page */
 
 function toggleDropdown() {
     var dropdown = document.querySelector('.dropdown');
     dropdown.classList.toggle('active');
-  }
+}
+
+/*closes the dropdown if user */
+
+// Add event listener to the document body
+document.addEventListener('click', function(event) {
+    var dropdown = document.querySelector('.dropdown');
+    var targetElement = event.target;
+
+    // Check if the clicked element is not within the dropdown
+    if (!dropdown.contains(targetElement)) {
+        // Close the dropdown if it's currently open
+        if (dropdown.classList.contains('active')) {
+            dropdown.classList.remove('active');
+        }
+    }
+});
